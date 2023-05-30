5bb550c76bd35c87e8d9f42f4e4b0b1728548f05
Nitin Ganwani <nitganwani@gmail.com>
2023-05-30 18:59:51 +0200

    Add sass folder

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,6 @@
 import { Header } from "../header/Header";
 import { List } from "../list/List";
 import { Footer } from "../footer/footer";
-import "../sass/styles.scss";
 
 export function App() {
   return (
