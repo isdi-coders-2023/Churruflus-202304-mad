4a8c8b38b1d35535dcffdf5549b4231757a70ffc
david <daviidgt03@gmail.com>
2023-05-31 10:43:33 +0200

    Update list component to card

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,17 +1,13 @@
 import { Header } from "../header/Header";
-import { List } from "../list/List";
+import { Card } from "../list/Card";
 import { Footer } from "../footer/footer";
-import { Card } from "../card/card";
 
 export function App() {
   return (
     <>
       <Header></Header>
       <main className="main-container">
-        <List></List>
-        <Card>
-          <Card item={Simpson.Ima} key={item.id}></Card>
-        </Card>
+        <Card></Card>
       </main>
       <Footer></Footer>
     </>
