67bf94f1268026e88b8d8d18b81e68e31f63ac53
Silvia <silvia.garbas@gmail.com>
2023-05-31 19:05:01 +0200

    Modified list.test

--- src/components/list/List.test.tsx
+++ src/components/list/List.test.tsx
@@ -1,35 +1,31 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import {List} from "./List"
+import { List } from "./List";
 import { Simpson } from "../../models/simpson";
 import { ContextStructure, AppContext } from "../../context/app.context";
 
-
 describe("Given List component", () => {
   describe("When it is intantiate", () => {
-    const simpson = [
-      {Nombre: "Bart Simpson", Estado: "Vivo"},
-      {Nombre: "Hommer Simpson", Estado: "Muerto"}
-    ] as Simpson [];
+    const characters = [
+      { Nombre: "Bart Simpson", Estado: "Vivo" },
+      { Nombre: "Hommer Simpson", Estado: "Muerto" },
+    ] as Simpson[];
 
     const value: ContextStructure = {
-      AppContext: {
-        simpson,
+      charactersContext: {
+        characters,
       },
     } as unknown as ContextStructure;
 
+    render(
+      <AppContext.Provider value={value}>
+        <List></List>
+      </AppContext.Provider>
+    );
 
- render(
-   <AppContext.Provider value={value}>
-     <List></List>
-   </AppContext.Provider>
- );
-
- test("Then it should contain an <li> element", () => {
-      const element = screen.getByRole("list");
+    test("Then it should contain an <li> element", () => {
+      const element = screen.getAllByRole("listitem");
       expect(element).toBeInTheDocument();
     });
   });
 });
-
-
