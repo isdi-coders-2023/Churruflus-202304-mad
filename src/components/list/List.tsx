87ee3b1b2289f23ceaabbedc22abca7f7cca3172
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-31 09:13:30 +0200

    Add styles to list

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AppContext } from "../../context/app.context";
+import styles from "./list.module.scss"
 
 export function List() {
   const {
@@ -7,11 +8,11 @@ export function List() {
   } = useContext(AppContext);
 
   return (
-    <ul className="characters">
+    <ul className={styles.characters}>
       {characters.map((item) => (
         <li key={item._id}>
           {item.Nombre}
-          <img src={item.Imagen}></img>
+          <img src={item.Imagen} width='150' height='260'></img>
         </li>
       ))}
     </ul>
