31869515721e598ca6bc60b2fb9fc4eaadfdaed7
Nitin Ganwani <nitganwani@gmail.com>
2023-05-31 17:10:29 +0200

    Fix header duplicated functions

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,24 +1,20 @@
-import Header from "../header/Header";
 import { Footer } from "../footer/footer";
-
 import { AppRoutes } from "../app.routes/app.routes";
-import { List } from "../list/List";
 import { Menu } from "../menu/Menu";
 import { Button } from "../button/Button";
+import { Header } from "../header/Header";
 
 export function App() {
-  const title = "The Simpsons API"
+  const title = "The Simpsons API";
   return (
     <>
-      <Header></Header>
-      <AppRoutes></AppRoutes>
       <Header title={title}>
         <Menu></Menu>
       </Header>
+      <AppRoutes></AppRoutes>
       <Button></Button>
       <main className="main-container"></main>
-      <List></List>
- <AppRoutes></AppRoutes>
+
       <Footer></Footer>
     </>
   );
