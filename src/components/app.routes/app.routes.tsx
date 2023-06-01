55f2be46e2ace02b792bce430c82f1798329214c
Nitin Ganwani <nitganwani@gmail.com>
2023-06-01 11:53:47 +0200

    Change import from errorpage in app.routes

--- src/components/app.routes/app.routes.tsx
+++ src/components/app.routes/app.routes.tsx
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router-dom";
 
 const List = lazy(() => import("../list/List"));
 const Form = lazy(() => import("../form/Form"));
-const ErrorPage = lazy(() => import("../error/Errorpage"));
 const DetailPage = lazy(() => import("../detail/Detail"));
 const ApiPrivate = lazy(() => import("../api.private/Api.private"));
+const ErrorPage = lazy(() => import("../error/Errorpage"));
 
 export function AppRoutes() {
   return (
