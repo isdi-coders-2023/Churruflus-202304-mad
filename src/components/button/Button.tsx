8327d2e7502f2b14d9f212203fec4edb86885831
Nitin Ganwani <nitganwani@gmail.com>
2023-06-01 10:40:47 +0200

    Add onclick for buttons to do pagination

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,10 +1,30 @@
+import { useContext } from "react";
 import "./Button.module.scss";
 import styles from "./button.module.scss";
+import { AppContext } from "../../context/app.context";
 export function Button() {
+  const {
+    charactersContext: { currentPage, setCurrentPage },
+  } = useContext(AppContext);
+
   return (
     <div className={styles.buttons}>
-      <button className={styles.text_button}>Anterior</button>
-      <button className={styles.text_button}>Siguiente</button>
+      <button
+        className={styles.text_button}
+        onClick={() => {
+          currentPage > 0 ? setCurrentPage(currentPage - 1) : 0;
+        }}
+      >
+        Anterior
+      </button>
+      <button
+        className={styles.text_button}
+        onClick={() => {
+          currentPage < 127 ? setCurrentPage(currentPage + 1) : 0;
+        }}
+      >
+        Siguiente
+      </button>
     </div>
   );
 }
