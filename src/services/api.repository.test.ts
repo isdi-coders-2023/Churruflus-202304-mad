0b2c4bb092080eca4ea61256e3378fe624bde8e9
Nitin Ganwani <nitganwani@gmail.com>
2023-06-02 10:48:54 +0200

    Add generic font family in styles

--- src/services/api.repository.test.ts
+++ src/services/api.repository.test.ts
@@ -57,38 +57,3 @@ describe("Given an ApiRepository", () => {
     });
   });
 });
-
-// describe("Given the class ApiRepository", () => {
-//   afterEach(() => {
-//     jest.clearAllMocks();
-//   });
-//   const repo = new ApiRepository();
-//   const mockSimpson = [
-//     { Nombre: "Bart Simpson", Estado: "Vivo" },
-//   ] as unknown as Simpson;
-//   global.fetch = jest.fn().mockResolvedValue({
-//     json: () => Promise.resolve(mockSimpson),
-//     ok: true,
-//   });
-//   describe("When it is instantiated implements SimpsonRepo", () => {
-//     test("then it should returns", async () => {
-//       const get = await repo.getAll("Hola", 2);
-//       expect(get).toEqual(mockSimpson);
-//     });
-//   });
-
-//   describe("Given the class ApiRepository", () => {
-//     describe("When it is instantiated implements SimpsonRepo", () => {
-//       const repo = new ApiRepository();
-//       global.fetch = jest.fn().mockResolvedValue({
-//         json: () => Promise.resolve(),
-//         ok: false,
-//         status: 400,
-//       });
-//       test("then it should returns", async () => {
-//         const get = await repo.getAll("Hola", 2);
-//         expect(get).toHaveProperty("status", 400);
-//       });
-//     });
-//   });
-// });
